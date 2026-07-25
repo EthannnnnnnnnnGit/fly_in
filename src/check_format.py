b95164ac787050e0c85c166f8d44972d8f85c3b2
Ethan El kerchi--Plouton <ethan@Host-001.lan>
2026-07-25 09:03:52 +0200

    custom error

--- src/check_format.py
+++ src/check_format.py
@@ -1,4 +1,5 @@
 import re
+from .error import FormatError
 
 
 class CheckFormat():
@@ -6,11 +7,26 @@ class CheckFormat():
         self.nb_drones_regex = r"^nb_drones:\s*(\d+)$"
 
     def is_drones_first_line(self, lines: list[str]):
-        for line in lines:
-            pass
+        self.start = True
+        for i, line in enumerate(lines):
+            if self.ignore_line(line):
+                continue
+            line = line.split("#")[0]
+            if self.get_number_drones(line):
+                continue
+            raise FormatError(f"[Line {i + 1}] Format error: Following line correspond to no parseable format.")          
     
     def get_number_drones(self, line: str) -> bool:
+        if not self.start:
+            return False
+
         if re.match(self.nb_drones_regex ,line):
             self.nb_drones
+            return True
+        raise ValueError("")
+    
+    def ignore_line(self, line: str) -> bool:
+        line = line.strip()
+        if line.startswith("#"):
             return True
         return False
