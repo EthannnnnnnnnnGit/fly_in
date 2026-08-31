34a16d4e38e05181bcf7b8cceebdc56a4ac0eb94
Ethan El kerchi--Plouton <e.elkerche.plouton@gmail.com>
2026-08-31 23:43:44 +0200

    visual testing for maps, drones not make yet

--- test.py
+++ test.py
@@ -5,8 +5,10 @@ from src.visual.file import RestrictedDirFile
 import os
 from pathlib import Path
 from PyQt6.Qt3DExtras import QCuboidMesh
-from PyQt6.Qt3DRender import QSceneLoader
+from PyQt6.QtGui import QQuaternion
+from PyQt6.Qt3DRender import QSceneLoader, QMesh, QRenderStateSet, QDepthTest, QColorMask
 from PyQt6.QtCore import QUrl
+import math
 
 
 class MainWindow(QWidget):
@@ -31,10 +33,7 @@ class MainWindow(QWidget):
         self.setup_overlay()
 
         self.hub_creator = HubsVisualizer(self.root)
-        self.hub_creator.create_hub((-15.0, 0.0, 0.0), "#a6e3a1")
-        self.hub_creator.create_hub((0.0, 0.0, -10.0), "#89b4fa")
-        self.hub_creator.create_hub((0.0, 0.0, 10.0), "#cba6f7")
-        self.hub_creator.create_hub((15.0, 0.0, 0.0), "#f38ba8")
+        self.hub_creator.create_maps()
 
     def setup_camera(self) -> None:
         camera = self.view3d.camera()
@@ -49,10 +48,16 @@ class MainWindow(QWidget):
 
     def setup_light(self):
         self.light = QEntity(self.root)
+
+        light_pos = QTransform()
+        light_pos.setTranslation(QVector3D(0.0, 50.0, 50.0))
+
         light = QPointLight(self.light)
-        light.setColor(QColor("#EFDD10"))
+        light.setColor(QColor("white"))
         light.setIntensity(1)
+
         self.light.addComponent(light)
+        self.light.addComponent(light_pos)
 
     def setup_overlay(self):
         toolbar = QHBoxLayout()
@@ -94,20 +99,124 @@ class MainWindow(QWidget):
 class HubsVisualizer():
     def __init__(self, root):
         self.root = root
+        self.define_grass_check()
+        self.define_water_check()
 
-    def create_hub(self, coordinates: tuple[float, float, float], color: str):
+    def create_maps(self):
+        data = [((0, 0), (15, 5)),
+                ((15, 5), (30, 0)),
+                ((0, 0), (15, -5)),
+                ((15, -5), (30, 0))]
+        for coordinate in data:
+            self.create_river(coordinate[0], coordinate[1])
+        self.create_hub((0, 0))
+        self.create_hub((15, 5))
+        self.create_hub((15, -5))
+        self.create_hub((30, 0))
+        self.make_grass()
+
+    def make_grass(self):
+        for x, z in [(x, z) for x in range(-5, 35) for z in range(-10, 10)]:
+            grass = QEntity(self.root)
+
+            mesh = QMesh()
+            mesh.setSource(QUrl.fromLocalFile("assets/grass.obj"))
+
+            transform = QTransform()
+            transform.setTranslation(QVector3D(x, 0, z))
+
+            grass.addComponent(mesh)
+            grass.addComponent(transform)
+            grass.addComponent(self.grass_check)
+
+    def create_hub(self, coordinates: tuple[float, float, float], color: str = "blue"):
         hub = QEntity(self.root)
+        x, z = coordinates
+        y = -0.4
+        scale = (5, 1, 5)
 
-        mesh = QSceneLoader()
-        mesh.setSource(QUrl.fromLocalFile("assets/water.glb"))
+        mesh = QMesh()
+        mesh.setSource(QUrl.fromLocalFile("assets/lake.obj"))
 
         transform = QTransform()
-        transform.setTranslation(QVector3D(*coordinates))
+        transform.setTranslation(QVector3D(x, y, z))
+        transform.setScale3D(QVector3D(*scale))
+
+        material = QPhongMaterial()
+        material.setDiffuse(QColor("blue"))
 
         hub.addComponent(mesh)
         hub.addComponent(transform)
+        hub.addComponent(material)
 
-        return hub
+        self.apply_mask((x, y + 0.1, z), (5, 2, 5), mesh)
+
+    def create_river(self, start: tuple[int, int], end: tuple[int, int]):
+        x1, z1 = start[0], -start[1]
+        x2, z2 = end[0], -end[1]
+
+        middle = ((x1 + x2) / 2, 0, (z1 + z2) / 2)
+        angle = math.degrees(math.atan2(z2 - z1, x2 - x1))
+        lenght = math.sqrt((x2 - x1) ** 2 + (z2 - z1) ** 2)
+
+        river = QEntity(self.root)
+
+        mesh = QMesh()
+        mesh.setSource(QUrl.fromLocalFile("assets/water.obj"))
+
+        transform = QTransform()
+        transform.setTranslation(QVector3D(*middle))
+        transform.setScale3D(QVector3D(lenght, 0.5, 1))
+        transform.setRotation(QQuaternion.fromAxisAndAngle(QVector3D(0, 1, 0), -angle))
+
+        material = QPhongMaterial()
+        material.setDiffuse(QColor("blue"))
+
+        river.addComponent(mesh)
+        river.addComponent(transform)
+        river.addComponent(material)
+
+        self.apply_mask(middle, (lenght, 1.3, 1), mesh, angle)
+
+    def apply_mask(self, coordinates, scale, mesh: QMesh, angle: int = 0):
+        mask = QEntity(self.root)
+
+        transform = QTransform()
+        transform.setTranslation(QVector3D(*coordinates))
+        transform.setScale3D(QVector3D(*scale))
+        transform.setRotation(QQuaternion.fromAxisAndAngle(QVector3D(0, 1, 0), -angle))
+
+        mask.addComponent(mesh)
+        mask.addComponent(transform)
+        mask.addComponent(self.mask_check)
+
+    def define_water_check(self):
+        self.mask_check = QPhongMaterial()
+
+        mask_color = QColorMask()
+        mask_color.setRedMasked(False)
+        mask_color.setGreenMasked(False)
+        mask_color.setBlueMasked(False)
+        mask_color.setAlphaMasked(False)
+
+        mask_depth = QDepthTest()
+        mask_depth.setDepthFunction(QDepthTest.DepthFunction.Less)
+
+        for technique in self.mask_check.effect().techniques():
+            for render_pass in technique.renderPasses():
+                render_pass.addRenderState(mask_color)
+                render_pass.addRenderState(mask_depth)
+
+    def define_grass_check(self):
+        self.grass_check = QPhongMaterial()
+        self.grass_check.setDiffuse(QColor("#267001"))
+
+        grass_depth = QDepthTest()
+        grass_depth.setDepthFunction(QDepthTest.DepthFunction.LessOrEqual)
+
+        for technique in self.grass_check.effect().techniques():
+            for render_pass in technique.renderPasses():
+                render_pass.addRenderState(grass_depth)
 
 
 if __name__ == "__main__":
